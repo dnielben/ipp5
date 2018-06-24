2f67c3f1515f48613666c306b8f07a2fac24cdd2
Johan Peña <jlozano@MacBook-Air-de-Juan.local>
2018-06-24 14:36:14 -0500

    Se ajusta menu mobil correctamente en todas las vistas - Se agrega manejo de usuarios en todas las vista

--- assets/firebase/loginFromIndex.js
+++ assets/firebase/loginFromIndex.js
@@ -15,9 +15,18 @@
     const btnLogout = document.getElementById('logout');
     const btnProfile = document.getElementById('profile');
 
+    const bntLoginMobile = document.getElementById('loginMobile');
+    const btnLogoutMobile = document.getElementById('logoutMobile');
+    const btnProfileMobile = document.getElementById('profileMobile');
+
     btnLogout.addEventListener('click', e => {
         firebase.auth().signOut();
-        console.log(e.messafe);
+        console.log(e.message);
+    });
+
+    btnLogoutMobile.addEventListener('click', e => {
+        firebase.auth().signOut();
+        console.log(e.message);
     });
 
     //Agregar un oyente de tiempo real de autenticación
@@ -27,11 +36,19 @@
             btnLoginRegister.classList.add('hide');
             btnLogout.classList.remove('hide');
             btnProfile.classList.remove('hide');
+            
+            bntLoginMobile.classList.add('hide');
+            btnProfileMobile.classList.remove('hide');
+            btnLogoutMobile.classList.remove('hide');
         } else {
             console.log('notLoggedIn');
             btnLoginRegister.classList.remove('hide');
             btnLogout.classList.add('hide');
             btnProfile.classList.add('hide');
+
+            bntLoginMobile.classList.remove('hide');
+            btnProfileMobile.classList.add('hide');
+            btnLogoutMobile.classList.add('hide');
         }
     });
 
